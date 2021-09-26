8f9f913efd4a39e128eee67fc15c2b1449786bb2
Daniel Vega <danielalexvega@gmail.com>
2021-09-26 16:29:17 -0500

    add general middleware for any wrong routes

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const problemsRoutes = require("./routes/problems-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/problems", problemsRoutes);
+
+app.use((req, res, next)=>{
+    const error = new HttpError("Could not find this route.", 404);
+    throw error;
+});
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
