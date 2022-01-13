f37f24a808422f170af82ca33229b9df7a5364d8
Daniel Vega <daniel.vega@pearson.com>
2022-01-12 22:52:35 -0600

    adding bcrypt to hash password

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -44,10 +45,21 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+      hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+      const error = new HttpError(
+          'Could not create user, please try again',
+          500
+      );
+      return next(error);
+  }
+  
   const createdUser = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
     image: req.file.path,
     problems: [],
   });
@@ -77,12 +89,31 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
       401
     );
     return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+      const error = new HttpError(
+          'Could not log you in. Please try your credentials and try again.',
+          500
+      );
+      return next(error);
+  }
+
+  if(!isValidPassword) {
+    const error = new HttpError(
+        "Invalid credentials, could not log you in.",
+        401
+      );
+      return next(error);
   }
 
   res.json({
