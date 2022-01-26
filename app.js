19794590dfa7aa66decbe2432d471d1bf1af80f6
Daniel Vega <danielalexvega@gmail.com>
2022-01-25 23:10:27 -0600

    added PORT env

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose
         `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.bpmz7.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`
     )
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch((err) => {
         console.log(err);
